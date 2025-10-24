bdd032767d64458dde578bc0bcd6fda9cb14d3c9
AthulKrishna <athulkrishnapv6@gmail.com>
2025-10-24 21:17:21 +0530

    feat: completed initial setup and docker configuration

---
+++ backend/services/api/src/test/java/com/zenith/ZenithBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
